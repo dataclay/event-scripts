285f8d4ef4283679dfcc3198ce05643482c1913f
blakmarkit <blakmarkit@gmail.com>
2017-12-07 16:59:04 -0600

    Updated post-job scripts
    Updated Windows and OSX post-job scripts and comments to reflect correct default argument order.

--- OSX/post-job.sh
+++ OSX/post-job.sh
@@ -5,22 +5,23 @@
 #   command as arguments.
 #
 #   Argument order is as follows for render operations after each job completes
-#       $1 => The value of the job's ID column if it has one, `null` if no ID value.
-#       $2 => The row index in the spreadsheet.  This is `null` when Bot is enabled.
+#       $1 => The row index in the spreadsheet.  This is always `null` when Bot is enabled.
+#       $2 => The value of the job's ID column if it has one, `null` if no ID value.
 #       $3 => The value of the job's devised output name.
 #       $4 => Absolute path to the final rendered file if it was rendered.
-#       $5 => Absolute path to the processed AE project file.
-#       $6 => Absolute path to the folder containing the processed AE project file.
-#       $7 => Absolute path to a .json file containing all job's column values
+#       $5 => Absolute path to the folder containing the rendered file.
+#       $6 => Absolute path to the processed AE project file.
+#       $7 => Absolute path to the folder containing the processed AE project file.
+#       $8 => Absolute path to a .json file containing all job's column values
 #
 #   Provided for your personal or commercial use by Dataclay, LLC.
 
-log="$6/post-job.log"
+log="$7/post-job.log"
 echo "-------- [TEMPLATER JOB] --------" >> "$log"
 echo "" >> "$log"
 echo "    Job completed on $(date)" >> "$log"
 echo "" >> "$log"
-echo "    Rendered job with ID \`"$1"\` to "$4 >> "$log"
+echo "    Rendered job with ID \`"$2"\` to "$4 >> "$log"
 echo "" >> "$log"
-echo "    Job details as JSON are found in file "$7 >> "$log"
+echo "    Job details as JSON are found in file "$8 >> "$log"
 echo "" >> "$log"
