ee539ba6ce4320536135d68e11a3d3585b0cd77e
Arie Stavchansky <arie@dataclay.com>
2021-07-13 10:27:46 -0500

    Example logging for spot checks
    Spot checking is a feature coming in the next release of Templater.  This is just a simple example script to log paths to spot checks.

---
+++ OSX/spot-logger.sh
@@ -0,0 +1,13 @@
+spot_log=$1/spots.log
+now=$2
+spotA=$3
+spotB=$4
+spotC=$5
+spotD=$6
+
+
+echo "\n----- SPOT CHECKS ON [ $now ] -----\n " >> $spot_log
+echo "\tSpot Check A => $spotA\n" >> $spot_log
+echo "\tSpot Check B => $spotB\n" >> $spot_log
+echo "\tSpot Check C => $spotC\n" >> $spot_log
+echo "\tSpot Check C => $spotD\n" >> $spot_log
